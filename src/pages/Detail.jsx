2ba002b1591084d1ab9f6f987b43c9329c91eee9
Jang Hyejin <hyeee513@naver.com>
2023-08-08 15:06:00 +0900

    Rename/Separate Post, Comments components

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,14 @@
+import Comments from 'components/detail/Comments';
 import Post from 'components/detail/Post';
 import React from 'react';
 
 const Detail = () => {
-  return <Post />;
+  return (
+    <>
+      <Post />
+      <Comments />
+    </>
+  );
 };
 
 export default Detail;
