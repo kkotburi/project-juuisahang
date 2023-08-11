3672e80a16de969f780dba40855036dd464c21dc
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-08-11 12:53:21 +0900

    Design/header/category

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useUserStore } from 'store';
 import { styled } from 'styled-components';
 
@@ -7,29 +7,78 @@ import LoginUtil from './LoginUtil';
 import LogoutUtil from './LogoutUtil';
 
 const Header = () => {
+  const location = useLocation();
   const navigate = useNavigate();
-
-  const clickCategory = (code) => {
-    navigate(`/category/${code}`);
-  };
 
   // 조회
   const currentUser = useUserStore((state) => state.currentUser);
   //   console.log('주스탠드 => ', currentUser);
 
+  const clickLogo = () => {
+    if (location.pathname === '/') {
+      window.location.reload();
+    } else {
+      navigate('/');
+    }
+  };
+
   return (
-    <div>
+    <HeaderContainer>
+      <HeaderImg src="/logo.png" onClick={clickLogo} />
+      <CategoryWrapper>
+        <CategoryLink to="/category/술자리 팁">술자리 팁</CategoryLink>
+        <CategoryLink to="/category/건배사">건배사</CategoryLink>
+        <CategoryLink to="/category/술 게임">술 게임</CategoryLink>
+        <CategoryLink to="/category/숙취해소법">숙취해소법</CategoryLink>
+      </CategoryWrapper>
       {currentUser ? <LoginUtil currentUser={currentUser} /> : <LogoutUtil />}
-      <div>
-        <button onClick={() => clickCategory('술자리 팁')}>술자리 팁</button>
-        <button onClick={() => clickCategory('건배사')}>건배사</button>
-        <button onClick={() => clickCategory('술 게임')}>술 게임</button>
-        <button onClick={() => clickCategory('숙취해소법')}>숙취해소법</button>
-      </div>
-    </div>
+    </HeaderContainer>
   );
 };
 
 export default Header;
 
-const Test = styled.main``;
+const HeaderContainer = styled.div`
+  height: 100px;
+  background-color: white;
+  border-bottom: solid 1px #969696;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HeaderImg = styled.img`
+  width: 80px;
+  height: auto;
+  background-color: black;
+
+  cursor: pointer;
+`;
+
+const CategoryWrapper = styled.div`
+  background-color: yellow;
+
+  width: 1000px;
+
+  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+`;
+
+const CategoryLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-size: 18px;
+
+  margin: 0px 40px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+  &:hover {
+    font-weight: 900;
+  }
+`;
--- src/components/header/LogoutUtil.jsx
+++ src/components/header/LogoutUtil.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { styled } from 'styled-components';
 
 const LogoutUtil = () => {
-  const navigate = useNavigate();
-
   return (
-    <div>
-      <button
-        onClick={() => {
-          navigate('/login');
-        }}
-      >
-        로그인
-      </button>
-    </div>
+    <AuthContainder>
+      <LoginLink to="/login">로그인</LoginLink>
+    </AuthContainder>
   );
 };
 
 export default LogoutUtil;
+
+const AuthContainder = styled.div`
+  background-color: pink;
+
+  width: 100px;
+`;
+
+const LoginLink = styled(Link)`
+  background-color: red;
+
+  text-decoration: none;
+  color: black;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+  &:hover {
+    font-weight: 900;
+  }
+`;
